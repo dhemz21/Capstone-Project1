0121a906c492343855f8e0a90b9c38147fa46e3a
Dhemler Omapas <omapasdhemz77@gmail.com>
2023-04-17 23:27:41 +0800

    updated on april 17

---
+++ dashboard_incharge/action/generate.php
@@ -0,0 +1,9 @@
+<?php
+// Fetch data from your MySQL table
+// Replace with your database connection and query
+require_once('../database/db_conn.php');
+
+$query = "SELECT title, subject, venue FROM incharge_add_event ORDER BY userID DESC"; // Replace 'id' with the name of your primary key column
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($result);
+?>
--- dashboard_incharge/pages/attendance-event.php
+++ dashboard_incharge/pages/attendance-event.php
@@ -4,6 +4,7 @@
 <?php
 
 include("library/attendance/call_function.php");
+require_once('action/generate.php');
 require_once("../database/db_conn.php");
 ?>
 
@@ -13,8 +14,9 @@ require_once("../database/db_conn.php");
   <section class="content">
     <div class="row mb-2 mt-2">
       <div class="col-12">
-        <button class="btn text-white" id="add" onclick="downloadPDF()"><i class="fas fa-download"></i> Download PDF</button>
+        <button class="btn text-white" id="generate"><i class="fas fa-download"></i> Download PDF</button>
         <button class="btn btn-danger" onclick="confirmDelete()"><i class="fas fa-trash-alt"></i> Delete Record</button>
+
       </div>
     </div>
     <div class="row">
@@ -61,10 +63,138 @@ require_once("../database/db_conn.php");
   </div>
   <!-- /.content -->
   
-  <script src="js/jspdf.js"></script>
+  <!-- <script src="js/jspdf.js"></script> -->
   <script src="js/colvis.js"></script>
   <script src="js/delete_record.js"></script>
 
+  <script>
+     // Attach click event to the Generate PDF button
+document.getElementById('generate').addEventListener('click', function() {
+  var table = document.getElementById("example1");
+
+    // Create a new jsPDF instance
+    var doc = new window.jspdf.jsPDF('portrait', 'pt', 'a4');
+
+      // Add logo to the top of the document
+      var logoImg = new Image();
+  logoImg.src = 'img/evsu.png';
+  logoImg.onload = function () {
+    var canvas = document.createElement('canvas');
+    canvas.width = logoImg.width;
+    canvas.height = logoImg.height;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(logoImg, 0, 0, logoImg.width, logoImg.height);
+    var logoDataUrl = canvas.toDataURL('image/png');
+
+    // Calculate position of the logo to center it
+    var logoWidth = 50; // Change this value to adjust the logo size
+    var logoHeight = logoWidth * logoImg.height / logoImg.width;
+    var logoX = (doc.internal.pageSize.width - logoWidth) / 2;
+    var logoY = 20;
+
+    doc.addImage(logoDataUrl, 'PNG', logoX, logoY, logoWidth, logoHeight);
+    addTitle(doc);
+    addMeeting(doc);
+    addTable(doc, table);
+    doc.save('attendance.pdf');
+  };
+
+});
+
+function addTitle(doc){
+  // Add title to the document
+  var title = 'Republic of the Philippines';
+  var titleFontSize = 12;
+  var titleWidth = doc.getStringUnitWidth(title.toUpperCase()) * titleFontSize / doc.internal.scaleFactor;
+  var titleX = (doc.internal.pageSize.width - titleWidth) / 2;
+  var titleY = 100;
+  doc.setFontSize(titleFontSize);
+  doc.setTextColor(0, 0, 0); // set title color to red
+  doc.text(title.toUpperCase(), titleX, titleY);00
+  doc.setFont('Helvetica');
+
+  // Add second title below the first one
+  var secondTitle = 'Eastern Visayas State University Ormoc City Campus ';
+  var secondTitleFontSize = 14;
+  var secondTitleWidth = doc.getStringUnitWidth(secondTitle.toUpperCase()) * secondTitleFontSize / doc.internal.scaleFactor;
+  var secondTitleX = (doc.internal.pageSize.width - secondTitleWidth) / 2;
+  var secondTitleY = titleY + 25;
+  doc.setFontSize(secondTitleFontSize);
+  doc.setTextColor(225, 0, 0); // set title color to black
+  doc.text(secondTitle.toUpperCase(), secondTitleX, secondTitleY);
+  doc.setFont('Helvetica');
+
+
+  // Add second title below the second one
+  var thirdTitle = 'Attendance Report';
+  var thirdTitleFontSize = 12;
+  var thirdTitleWidth = doc.getStringUnitWidth(thirdTitle.toUpperCase()) * thirdTitleFontSize / doc.internal.scaleFactor;
+  var thirdTitleX = (doc.internal.pageSize.width - thirdTitleWidth) / 2;
+  var thirdTitleY = titleY + 70;
+  doc.setFontSize(thirdTitleFontSize);
+  doc.setTextColor(0, 0, 0); // set title color to black
+  doc.text(thirdTitle.toUpperCase(), thirdTitleX, thirdTitleY);
+  doc.setFont('Helvetica');
+
+
+    // Add date below the second title
+    var date = new Date().toLocaleDateString();
+    var formattedDate = "Date: " + date;
+    var dateFontSize = 12;
+    var dateWidth = doc.getStringUnitWidth(formattedDate) * dateFontSize / doc.internal.scaleFactor;
+    var leftMargin = 40; // set the left margin to 40 units
+    var bottomMargin = 20; // set the bottom margin to 20 units
+    var dateX = leftMargin;
+    var dateY = thirdTitleY + 25;
+    doc.setFontSize(dateFontSize);
+    doc.setTextColor(0, 0, 0); // set title color to black
+    doc.text(formattedDate, dateX, dateY);
+    doc.setFont('Helvetica');
+    doc.setFontSize(10);
+}
+
+function addMeeting(doc){
+  // Fetch data from your MySQL table
+  var title = 'Title: ' + '<?php echo strtoupper($row["title"]); ?>'; // Replace with your column name
+  var subject = 'Subject: ' + '<?php echo strtoupper($row["subject"]); ?>'; // Replace with your column name
+  var venue = 'Venue: ' + '<?php echo strtoupper($row["venue"]); ?>'; // Replace with your column name
+
+  var leftMargin = 40;
+  var topMargin = 215;
+  var titleFontSize = 12;
+  doc.setFont('Helvetica');
+  doc.setFontSize(12);
+
+  // Add the title, subject, and venue to the PDF
+  doc.text(title, leftMargin, topMargin);
+  doc.text(subject, leftMargin, topMargin + 20);
+  doc.text(venue, leftMargin, topMargin + 40);
+}
+
+function addTable(doc, table) {
+  // Add table to the document
+  var rows = table.getElementsByTagName("tr");
+  var tableData = [];
+  for (var i = 0; i < rows.length; i++) {
+    var columns = rows[i].getElementsByTagName("td");
+    var rowData = [];
+    for (var j = 0; j < columns.length; j++) {
+      rowData.push(columns[j].innerText);
+    }
+    tableData.push(rowData);
+  }
+  doc.autoTable({
+    startY: 280,
+    head: [['Lastname', 'Firstname', 'Type', 'Time In']],
+    headStyles: {
+      fillColor: [183, 28, 28], // set table header background color to maroon
+      textColor: [255, 255, 255] // set table header font color to white
+    },
+    body: tableData
+  });
+}
+
+  </script>
 </body>
 
 </html>
